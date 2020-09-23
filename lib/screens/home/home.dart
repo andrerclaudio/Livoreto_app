df97aef7f89b9342ed34b65511902be8ae7c39f6
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2020-09-23 14:44:19 -0300

    Starting the Home Screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class OurHomeScreen extends StatelessWidget {
@@ -11,11 +13,51 @@ class OurHomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Row(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Add a new Book',
-        child: Icon(Icons.add),
+      body: FetchStatus(),
+    );
+  }
+}
+
+class FetchStatus extends StatefulWidget {
+  void sendData() {
+    Socket.connect('192.168.0.163', 5000).then((socket) {
+      print('Connected to: '
+          '${socket.remoteAddress.address}:${socket.remotePort}');
+      socket.write('I am a message from a Mobile Application');
+      socket.destroy();
+    });
+  }
+
+  @override
+  _FetchStatusState createState() => _FetchStatusState();
+}
+
+class _FetchStatusState extends State<FetchStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(8.0),
+      height: 128,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey,
+        border: Border.all(),
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      ),
+      child: Center(
+        child: FlatButton(
+          color: Colors.blue,
+          textColor: Colors.black,
+          disabledColor: Colors.grey,
+          disabledTextColor: Colors.black,
+          padding: EdgeInsets.all(8.0),
+          splashColor: Colors.blueAccent,
+          onPressed: widget.sendData,
+          child: Text(
+            "Press to get status!",
+            style: TextStyle(fontSize: 16.0),
+          ),
+        ),
       ),
     );
   }
